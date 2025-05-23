08d4cba36a2f57a9ccb9b2475844ddd28a9d69f8
neonfuz <neonfuz@gmail.com>
2025-05-23 01:01:12 -0500

    update nixos 24.11->25.05

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   description = "The best way to run an Urbit ship";
 
-  inputs.nixpkgs.url = "nixpkgs/nixos-24.11";
+  inputs.nixpkgs.url = "nixpkgs/nixos-25.05";
 
   outputs = { self, nixpkgs, ... }:
     let
